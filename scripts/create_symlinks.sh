d7f191fa84d4ef62b481c653a02453206ea42e2b
Jack Hunt <jackmileshunt@gmail.com>
2022-12-21 23:57:57 +0000

    Cleanup

--- scripts/create_symlinks.sh
+++ scripts/create_symlinks.sh
@@ -2,30 +2,15 @@
 
 source ./scripts/utils.sh
 
-PD="portage"
-SYS_PD="/etc/portage"
-
-ACC_KWD="package.accept_keywords"
-LIC="package.license"
-MASK="package.mask"
-USE="package.use"
 EMACS=".emacs.d"
 ZSHRC=".zshrc"
 CONFIG=".config"
-XRES=".Xresources"
-WALLPAPERS="wallpapers"
 
 declare -A LINKS;
 LINKS=(
-    ["${PD}/${ACC_KWD}"]="${SYS_PD}/${ACC_KWD}"
-    ["${PD}/${LIC}"]="${SYS_PD}/${LIC}"
-    ["${PD}/${MASK}"]="${SYS_PD}/${MASK}"
-    ["${PD}/${USE}"]="${SYS_PD}/${USE}"
     ["${EMACS}"]="${HOME}/${EMACS}"
     ["${ZSHRC}"]="${HOME}/${ZSHRC}"
     ["${CONFIG}"]="${HOME}/${CONFIG}"
-    ["${XRES}"]="${HOME}/${XRES}"
-    ["${WALLPAPERS}"]="${HOME}/${WALLPAPERS}"
 )
 
 for src in "${!LINKS[@]}"; do
@@ -38,3 +23,4 @@ for src in "${!LINKS[@]}"; do
         create_symlink "$(pwd)/$src" $dst;
     fi
 done
+
