85168083cd75941981f0724bce67a383824065b3
Jack Hunt <JackHunt@users.noreply.github.com>
2024-04-09 20:25:41 +0100

    delete old configs

--- .config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-killall -q polybar
-
-polybar --config=$HOME/.config/polybar/config.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
-#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-
-echo "Bars launched..."
