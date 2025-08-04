3ec5b3f4bba6870af8c0beedab2500c816a947ce
Jack Hunt <jackmileshunt@gmail.com>
2025-08-04 14:35:54 +0100

    Create jupyter_extensions.sh

---
+++ jupyter_extensions.sh
@@ -0,0 +1,8 @@
+pip install \
+    rose-pine-jupyterlab \
+    jupyterlab-git \
+    jupyterlab-quarto \
+    jupyterlab-rainbow-brackets \
+    jupyterlab-spreadsheet-editor
+
+jupyter labextension install jupyterlab-emacskeys
